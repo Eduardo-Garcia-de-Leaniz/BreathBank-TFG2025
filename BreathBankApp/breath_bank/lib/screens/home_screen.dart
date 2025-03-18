ddb01e7be06ec48b787c5afa12f2f68946dab2ae
EduardoGarciadeLeaniz <118168586+EduardoGarciadeLeaniz@users.noreply.github.com>
2025-03-18 19:08:29 +0100

    Modificaciones página principal

--- BreathBankApp/breath_bank/lib/screens/home_screen.dart
+++ BreathBankApp/breath_bank/lib/screens/home_screen.dart
@@ -6,27 +6,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BreathBank'),
-        backgroundColor: const Color.fromARGB(255, 27, 180, 235),
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        //padding: const EdgeInsets.all(10),
-        width: double.infinity,
-        height: 350,
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 180, 46, 46),
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                'BreathBank',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Arial',
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
-        child: const Center(child: Text('Bienvenido a BreathBank!')),
-      ),
-      backgroundColor: const Color.fromARGB(255, 202, 226, 236),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: const Color.fromARGB(255, 27, 180, 235),
       ),
     );
   }
