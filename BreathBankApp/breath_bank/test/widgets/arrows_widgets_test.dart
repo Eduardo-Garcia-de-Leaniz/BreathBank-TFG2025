11bb921d747295b7729c6394f3e6fe3a00aac8d4
EduardoGarciadeLeaniz <118168586+EduardoGarciadeLeaniz@users.noreply.github.com>
2025-06-03 22:08:56 +0200

    Más test widgets

---
+++ BreathBankApp/breath_bank/test/widgets/arrows_widgets_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:breath_bank/widgets/arrow_previous_symbol.dart';
+import 'package:breath_bank/widgets/arrow_next_symbol.dart';
+
+void main() {
+  testWidgets('ArrowPreviousSymbol muestra el icono de flecha atrás', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(home: Stack(children: const [ArrowPreviousSymbol()])),
+    );
+
+    // Busca el icono de flecha atrás
+    expect(find.byIcon(Icons.arrow_back_ios), findsOneWidget);
+  });
+
+  testWidgets('ArrowNextSymbol muestra el icono de flecha adelante', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      MaterialApp(home: Stack(children: const [ArrowNextSymbol()])),
+    );
+
+    // Busca el icono de flecha adelante
+    expect(find.byIcon(Icons.arrow_forward_ios), findsOneWidget);
+  });
+}
